904d74af8bb89b93b26e9eaa433395cfee95fdec
Camillanrgaard <camilla.nrgaard@yahoo.dk>
2023-10-25 15:09:31 +0200

    Footer mobile

--- footer.php
+++ footer.php
@@ -1,3 +1,33 @@
+<footer>
+
+    <div class="footer-grid">
+        <div class="footer-address">
+            <a target=_blank href="https://maps.app.goo.gl/YGS1QfN8hQckBftU9">Vilhelmsborgvej 1, 8320 Mårslet</a>
+        </div>
+        <div class="footer-mail">
+            <a target=_blank href="mailto:aska.bestyrelse@gmail.com">aska.bestyrelse@gmail.com</a>
+        </div>
+        <div class="footer-cvr">
+            <P>CVR nr. 39561409</P>
+        </div>
+        <div class="footer-fb-logo">
+            <a target=_blank href="https://www.facebook.com/groups/1389413571362415"><img class="some-icon"
+                    alt="Facebook logo"
+                    src="<?php echo get_stylesheet_directory_uri(); ?>/assets/media/facebook-logo.svg" /></a>
+        </div>
+        <div class="footer-ig-icon">
+            <a target=_blank href="https://www.instagram.com/islaenderklubben.aska/"><img class="some-icon"
+                    alt="Instagram logo"
+                    src="<?php echo get_stylesheet_directory_uri(); ?>/assets/media/instagram-logo.svg" /></a>
+        </div>
+    </div>
+    <div class="footer-credits">
+        <p>©2023 ASKA ISLANDSHESTEKLUB</p>
+        <p>M.WENNERWALD OG C.NØRGAARD</p>
+    </div>
+</footer>
+
 <?php wp_footer(); ?>
 </body>
+
 </html>
