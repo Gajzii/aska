9060c863bf247f5853fdfbf13a2fc335d616c766
Maja Blom Wennerwald Nielsen <70579734+Gajzii@users.noreply.github.com>
2023-11-01 10:19:01 +0100

    opstart

--- template-calendar.php
+++ template-calendar.php
@@ -4,4 +4,12 @@
 ?>
 
 <?php get_header();?>
+
+<div class="secondary-hero">
+    <h1 class="secondary-hero-content">Kalender</h1>
+</div>
+<div class="calendar-select-section">
+
+</div>
+
 <?php get_footer(); ?>
