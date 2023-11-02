b5ec6041c900eea794f9ed14cc9a93127580c1ec
Camillanrgaard <camilla.nrgaard@yahoo.dk>
2023-11-02 16:49:01 +0100

    Rettelser<33

--- partials/components/sections/sections-membership-benefits.php
+++ partials/components/sections/sections-membership-benefits.php
@@ -6,12 +6,14 @@
                 flere
                 oplysninger om vores forskellige medlemsfordele nedenfor.</p>
         </div>
-        <div class="membership-benefits-cards">
+        <div class="prev-next-btns">
 
-            <?php if ( have_rows('membership_benefits') ) : ?>
-            <div class="cards-container">
-                <?php while ( have_rows('membership_benefits') ) : the_row(); ?>
-                <?php
+            <div class="membership-benefits-cards membership-benefits-cards-grid">
+
+                <?php if ( have_rows('membership_benefits') ) : ?>
+                <div class="cards-container">
+                    <?php while ( have_rows('membership_benefits') ) : the_row(); ?>
+                    <?php
                 $membership_benefits_discount = get_sub_field('membership_benefits_discount');
                 $membership_benefits_heading = get_sub_field('membership_benefits_heading');
                 $read_more_btn = get_sub_field('read_more_btn');
@@ -19,46 +21,44 @@
                 $membership_benefits_description = get_sub_field('membership_benefits_description');
             ?>
 
-                <div class="membership-benefits-card">
-                    <div class="benefits-icon-border">
-                        <div class="benefits-icon-bg">
-                            <h3><?= $membership_benefits_discount?>%</h3>
-                        </div>
-                    </div>
-                    <div class="benefits-card-bg">
-                        <h4><?= $membership_benefits_heading?></h4>
-                        <div class="benefits-btn">
-                            <div class="secondary-btn-border">
-                                <button class="readMore_multi secondary-btn btn-text-secondary">
-                                    Læs mere
-                                    <img class="arrow-icon" alt="Pil ikon til højre"
-                                        src="<?php echo get_stylesheet_directory_uri(); ?>/assets/media/arrow.svg" />
-                                </button>
+                    <div class="membership-benefits-card">
+                        <div class="benefits-icon-border">
+                            <div class="benefits-icon-bg">
+                                <h3><?= $membership_benefits_discount?>%</h3>
                             </div>
                         </div>
-                        <div class="benefits-card-img-overlay">
-                            <img class="benefits-card-img" src="<?php echo $membership_benefits_img['url']; ?>" />
+                        <div class="benefits-card-bg">
+                            <h4><?= $membership_benefits_heading?></h4>
+                            <div class="benefits-btn">
+                                <div class="secondary-btn-border">
+                                    <button class="readMore_multi secondary-btn btn-text-secondary">
+                                        Læs mere
+                                        <img class="arrow-icon" alt="Pil ikon til højre"
+                                            src="<?php echo get_stylesheet_directory_uri(); ?>/assets/media/arrow.svg" />
+                                    </button>
+                                </div>
+                            </div>
+                            <div class="benefits-card-img-overlay">
+                                <img class="benefits-card-img" src="<?php echo $membership_benefits_img['url']; ?>" />
+                            </div>
                         </div>
                     </div>
+                    <?php endwhile; ?>
+                    <?php endif; ?>
                 </div>
-                <?php endwhile; ?>
-                <?php endif; ?>
             </div>
-        </div>
-    </div>
-    <div class="prev-next-btns">
-        <div class="arrow-btn-border">
-            <button class="arow-btn prev-btn">
-                <img class="arrow-icon-left" alt="Pil ikon til venstre"
-                    src="<?php echo get_stylesheet_directory_uri(); ?>/assets/media/arrow-left.svg" />
-            </button>
-        </div>
-
-        <div class="arrow-btn-border">
-            <button class="arow-btn next-btn">
-                <img class="arrow-icon-right" alt="Pil ikon til venstre"
-                    src="<?php echo get_stylesheet_directory_uri(); ?>/assets/media/arrow.svg" />
-            </button>
+            <div class="arrow-btn-border arrow-icon-left-grid">
+                <button class="arow-btn prev-btn">
+                    <img class="arrow-icon-left" alt="Pil ikon til venstre"
+                        src="<?php echo get_stylesheet_directory_uri(); ?>/assets/media/arrow-left.svg" />
+                </button>
+            </div>
+            <div class="arrow-btn-border arrow-icon-right-grid">
+                <button class="arow-btn next-btn">
+                    <img class="arrow-icon-right" alt="Pil ikon til venstre"
+                        src="<?php echo get_stylesheet_directory_uri(); ?>/assets/media/arrow.svg" />
+                </button>
+            </div>
         </div>
     </div>
 </div>
