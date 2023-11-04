2d6eb5e7c76583fee3b3e289bb8de51c7e660f75
Maja Blom Wennerwald Nielsen <70579734+Gajzii@users.noreply.github.com>
2023-11-05 00:00:40 +0100

    det virker ikke

--- functions.php
+++ functions.php
@@ -1,10 +1,59 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 // Load function partials
 require 'functions/navigation.php';
 require 'functions/scripts.php';
 require 'functions/utilities.php';
 require 'functions/aska_calendar.php';
+
+
+
+// Register a recurring check for past events
+add_action('init', 'register_categorize_past_events');
+function register_categorize_past_events()
+{
+    // Make sure this event hasn't already been scheduled
+    if (!wp_next_scheduled('categorize_past_events')) {
+
+        // Schedule the event
+        wp_schedule_event(time(), 'hourly', 'categorize_past_events');
+    }
+}
+
+// Apply past-event category to past events
+add_action('categorize_past_events', 'categorize_past_events');
+function categorize_past_events()
+{
+    error_log("Categorization process started"); // Debug statement
+
+    // Search for events that aren't yet marked as past events but are in the past
+    $args = array(
+        'post_type' => 'kalender',
+        'nopaging' => true,
+        'category__not_in' => array(10), // Replace with your afholdt category ID
+        'meta_key' => 'calendar-date', // Replace with your ACF Date Picker field key
+        'meta_value' => date('m-d-Y'), // Compare with today in 'm-d-Y' format
+        'meta_compare' => '<', // Less than means it's in the past
+    );
+
+    $events = get_posts($args);
+
+    // Add the 'afholdt' category to each matching event
+    if ($events) {
+        error_log("Found " . count($events) . " events to categorize"); // Debug statement
+        foreach ($events as $event) {
+            wp_set_post_categories($event->ID, array(10), 'category', true); // Change '32' to your 'afholdt' category ID
+            error_log("Categorized event ID " . $event->ID); // Debug statement
+        }
+    } else {
+        error_log("No events found to categorize"); // Debug statement
+    }
+}
+
+
 
 ?>
 
--- template-calendar.php
+++ template-calendar.php
@@ -19,6 +19,9 @@ if (isset($_GET['category'])) {
 $args = array(
     'post_type' => 'kalender',
     'posts_per_page' => -1,
+    'meta_key' => 'calendar-date', // Specify the custom field key for ordering
+    'orderby' => 'meta_value',  // Order by the custom field value
+    'order' => 'DESC',         // Order in descending order
 );
 
 if (!empty($selected_categories)) {
@@ -61,38 +64,94 @@ echo '<div class="calendar-select-section">';
     echo '</div>'; 
 echo '</div>';
 
-$timestamp = strtotime(get_field('calendar-date'));
-$day = date('d', $timestamp);
-$mon = date('m', $timestamp);
-$year = date('Y', $timestamp);
-
+echo '<div class="page-margin event-cards-container">';
 if ($custom_query->have_posts()) {
-    echo '<div class="membership-benefits-card">';
+    
     while ($custom_query->have_posts()) {
         $custom_query->the_post();
-        echo '<div class="benefits-icon-border">';
-            echo '<div class="benefits-icon-bg">';
-                echo '<h3>' . get_field('calendar-date-day') . '</h3>';
-                echo '<h3>' . get_field('calendar-date-month') . '</h3>';
-                echo '<h3>' . get_field('calendar-date-year') . '</h3>';
-                echo '<h3>' . get_field('calendar-date') . '</h3>';
+        echo '<div class="membership-benefits-card event-card">';
+
+            echo '<div class="benefits-icon-border event-card-border">';
+                echo '<div class="benefits-icon-bg">';
+
+                    $timestamp = strtotime(get_field('calendar-date'));
+
+                    $day = wp_date('j', $timestamp);
+                    $mon = wp_date('M', $timestamp);
+                    $year = wp_date('Y', $timestamp);
+                    echo '<div class="event-date">';
+                        echo '<p class="event-dag">' . $day . '.</p>';
+                        echo '<p class="event-maaned">' . $mon . '</p>';
+                        echo '<p class="event-aar">' . $year . '</p>';
+                    echo '</div>';
+
+                echo '</div>';
             echo '</div>';
-        echo '</div>';
-        echo '<div class="benefits-card-bg">';
+
+        echo '<div class="benefits-card-bg event-card-bg">';
             echo '<h4>' . get_the_title() . '</h4>';
-            echo '<div class="benefits-btn">';
+            echo '<div class="benefits-btn event-btn">';
                 echo '<div class="secondary-btn-border">';
                     echo '<button class="readMore_multi secondary-btn btn-text-secondary">Læs mere</button>';
                 echo '</div>';
             echo '</div>';
         echo '</div>';
+        echo '</div>';
     }
-    echo '</div>';
+    
 } else {
     echo 'No posts found.';
 }
 
+// schedule
+
+ add_action('wp_insert_post', 'update_event_categories');
+
+function update_event_categories($post_id)
+{
+    $post = get_post($post_id);
+
+    if ($post->post_type != 'kalender') { // Change 'kalender' to your custom post type
+        return;
+    }
+
+    // Check if it's a revision and get the parent post ID
+    $parent = wp_is_post_revision($post_id);
+    if ($parent) {
+        $post_id = $parent;
+    }
+
+    // Get the date from your ACF Date Picker field (replace 'event-date' with your field key)
+    $event_date = get_field('calendar-date', $post_id);
+
+    if (!$event_date) {
+        return; // No event date, so nothing to categorize
+    }
+
+    $today = current_time('m-d-Y'); // Get the current date in 'm-d-Y' format
+    echo $today;
+
+    if (strtotime($event_date) < strtotime($today)) {
+        // The event date is in the past, so categorize it
+        $categories = wp_get_post_categories($post_id);
+
+        // Add the 'afholdt' category to the event
+        $afholdt_category = get_term_by('name', 'afholdt', 'category');
+        if ($afholdt_category) {
+            array_push($categories, $afholdt_category->term_id);
+            wp_set_post_categories($post_id, $categories);
+        }
+    }
+}
+
+
+echo '</div>';
 wp_reset_postdata();
+
+
+ 
+
+
 ?>
 
 
