1fb69ba6c328cca26767139a08092fdfc53260bd
Maja Blom Wennerwald Nielsen <70579734+Gajzii@users.noreply.github.com>
2023-11-05 11:21:18 +0100

    lidt mere styling

--- template-calendar.php
+++ template-calendar.php
@@ -9,25 +9,26 @@
     <h1 class="secondary-hero-content">Kalender</h1>
 </div>
 
+<!-- ---------------------- Filter Checkboxes --------------------- -->
 <?php
-$selected_categories = array(); // Initialize an array to store the selected categories
+$selected_categories = array();
 
 if (isset($_GET['category'])) {
-    $selected_categories = (array)$_GET['category']; // Get the selected categories from the URL parameter
+    $selected_categories = (array)$_GET['category'];
 }
 
 $args = array(
     'post_type' => 'kalender',
     'posts_per_page' => -1,
-    'meta_key' => 'calendar-date', // Specify the custom field key for ordering
-    'orderby' => 'meta_value',  // Order by the custom field value
-    'order' => 'DESC',         // Order in descending order
+    'meta_key' => 'calendar-date',
+    'orderby' => 'meta_value',
+    'order' => 'DESC',
 );
 
 if (!empty($selected_categories)) {
     $args['tax_query'] = array(
         array(
-            'taxonomy' => 'category', // Replace with the taxonomy you're using
+            'taxonomy' => 'category',
             'field'    => 'slug',
             'terms'    => $selected_categories,
         ),
@@ -36,10 +37,9 @@ if (!empty($selected_categories)) {
 
 $custom_query = new WP_Query($args);
 
-// Display category filter checkboxes
 $categories = get_terms(array(
-    'taxonomy' => 'category', // Replace with your taxonomy name
-    'hide_empty' => false, // Show even empty categories
+    'taxonomy' => 'category',
+    'hide_empty' => false,
 ));
 
 echo '<div class="calendar-select-section">';
@@ -64,96 +64,83 @@ echo '<div class="calendar-select-section">';
     echo '</div>'; 
 echo '</div>';
 
+// ---------------------- Event Cards --------------------- //
 echo '<div class="page-margin event-cards-container">';
 if ($custom_query->have_posts()) {
     
     while ($custom_query->have_posts()) {
         $custom_query->the_post();
-        echo '<div class="membership-benefits-card event-card">';
+        ?>
 
-            echo '<div class="benefits-icon-border event-card-border">';
-                echo '<div class="benefits-icon-bg">';
+        <div class="membership-benefits-card event-card">
 
-                    $timestamp = strtotime(get_field('calendar-date'));
+            <div class="benefits-icon-border event-card-border">
+                <div class="benefits-icon-bg">
 
-                    $day = wp_date('j', $timestamp);
-                    $mon = wp_date('M', $timestamp);
-                    $year = wp_date('Y', $timestamp);
-                    echo '<div class="event-date">';
-                        echo '<p class="event-dag">' . $day . '.</p>';
-                        echo '<p class="event-maaned">' . $mon . '</p>';
-                        echo '<p class="event-aar">' . $year . '</p>';
-                    echo '</div>';
+                    <?php $timestamp = strtotime(get_field('calendar-date'));
+                        $day = wp_date('j', $timestamp);
+                        $mon = wp_date('M', $timestamp);
+                        $year = wp_date('Y', $timestamp);
+                    ?>
 
-                echo '</div>';
-            echo '</div>';
+                    <div class="event-date">
+                        <p class="event-dag"><?= $day?></p>
+                        <p class="event-maaned"><?=$mon?></p>
+                        <p class="event-aar"><?=$year?></p>
+                    </div>
 
-        echo '<div class="benefits-card-bg event-card-bg">';
-            echo '<h4>' . get_the_title() . '</h4>';
-            echo '<div class="benefits-btn event-btn">';
-                echo '<div class="secondary-btn-border">';
-                    echo '<button class="readMore_multi secondary-btn btn-text-secondary">Læs mere</button>';
-                echo '</div>';
-            echo '</div>';
-        echo '</div>';
-        echo '</div>';
+                </div>
+            </div>
+
+            <div class="benefits-card-bg event-card-bg">
+                <h4><?=get_the_title()?></h4>
+                <div class="benefits-btn event-btn">
+                    <div class="secondary-btn-border">
+                        <button class="readMore_multi secondary-btn btn-text-secondary">Læs mere<img class="arrow-icon" alt="Pil ikon til højre"
+                            src="<?php echo get_stylesheet_directory_uri(); ?>/assets/media/arrow.svg" /></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- ---------------------- Event Modal --------------------- -->
+        <div class="modal modal_multi">
+            <div class="modal-flex">
+
+                <div class="benefits-icon-border modal-discount event-card-border">
+                    <div class="benefits-icon-bg">
+                        <div class="event-date">
+                            <p class="event-dag"><?= $day?></p>
+                            <p class="event-maaned"><?=$mon?></p>
+                            <p class="event-aar"><?=$year?></p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="modal-content">
+                    <div class="modal-padding">
+                        <span class="close_multi">
+                            <img class="close-icon" alt="Luk modal ikon" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/media/close-icon.svg" />
+                        </span>
+                        <div class="modal-text">
+                            <h4><?=get_the_title()?></h4>
+                            <p><?=get_the_content()?></p>
+                                
+                            <a href="<?=get_field('calendar-sporti-link')?>">Gå til Sporti</a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    <?php
     }
-    
-} else {
-    echo 'No posts found.';
-}
-
-// schedule
-
- add_action('wp_insert_post', 'update_event_categories');
-
-function update_event_categories($post_id)
-{
-    $post = get_post($post_id);
-
-    if ($post->post_type != 'kalender') { // Change 'kalender' to your custom post type
-        return;
-    }
-
-    // Check if it's a revision and get the parent post ID
-    $parent = wp_is_post_revision($post_id);
-    if ($parent) {
-        $post_id = $parent;
-    }
-
-    // Get the date from your ACF Date Picker field (replace 'event-date' with your field key)
-    $event_date = get_field('calendar-date', $post_id);
-
-    if (!$event_date) {
-        return; // No event date, so nothing to categorize
-    }
-
-    $today = current_time('m-d-Y'); // Get the current date in 'm-d-Y' format
-    echo $today;
-
-    if (strtotime($event_date) < strtotime($today)) {
-        // The event date is in the past, so categorize it
-        $categories = wp_get_post_categories($post_id);
-
-        // Add the 'afholdt' category to the event
-        $afholdt_category = get_term_by('name', 'afholdt', 'category');
-        if ($afholdt_category) {
-            array_push($categories, $afholdt_category->term_id);
-            wp_set_post_categories($post_id, $categories);
+        } else {
+            echo 'Der er ingen begivenheder i denne kategori.';
         }
-    }
-}
-
-
-echo '</div>';
-wp_reset_postdata();
-
-
- 
-
-
-?>
-
-
+        echo '</div>';
+        wp_reset_postdata();
+    ?>
 <div style="height: 1000px"></div>
 <?php get_footer();?>
