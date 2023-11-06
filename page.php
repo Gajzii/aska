3a77caf9c18452233056a406dfac46d43c82ffad
Camillanrgaard <camilla.nrgaard@yahoo.dk>
2023-11-06 12:26:12 +0100

    Page fortsat

--- page.php
+++ page.php
@@ -1,5 +1,14 @@
 <?php $page_hero_img = get_field('page_hero_img'); ?>
 
+<?php get_header(); ?>
+
+<!-- PAGE HERO -->
+<div class="page-hero-container">
+    <img class="page-hero" src="<?php echo $page_hero_img['url']; ?>" />
+    <h1 class="page-hero-content"><?= get_the_title(); ?></h1>
+</div>
+
+<!-- PAGE CONTENT -->
 <?php if ( have_rows('page_cards') ) : ?>
 <?php while ( have_rows('page_cards') ) : the_row(); ?>
 <?php
@@ -9,22 +18,25 @@
                 $page_cards_text = get_sub_field('page_cards_text');
             ?>
 
-<?php get_header(); ?>
-
-<div class="">
-    <img class="page-hero" src="<?php echo $page_hero_img['url']; ?>" />
-    <?= get_the_title(); ?>
-</div>
-
 <div class="page-margin">
+    <div class="page-cards-flex">
+        <div class="page-card" tabindex="0" ontouchmove=""
+            style="background-image: url('<?php echo $page_cards_bg_img['url']; ?>');">
+            <div class="page-card-heading" tabindex="0" ontouchmove="">
+                <h4>
+                    <?= $page_cards_heading ?>
+                </h4>
+                <div class="page-card-details">
+                    <p class="page-card-description"><?= $page_cards_description ?></p>
 
-
-
+                    <?php get_template_part( 'partials/components/parts/parts','secondary-btn' ); ?>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
-
-
-<?php get_footer(); ?>
 
 <?php endwhile; ?>
 <?php endif; ?>
+<?php get_footer(); ?>
