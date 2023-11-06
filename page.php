8674a3963ca576904e7537d2669b1f1ead96f5b6
Camillanrgaard <camilla.nrgaard@yahoo.dk>
2023-11-06 10:40:08 +0100

    Opstrart på undersider

--- page.php
+++ page.php
@@ -1,11 +1,32 @@
+<?php $page_hero_img = get_field('page_hero_img'); ?>
+
+
+
+<?php if ( have_rows('page_cards') ) : ?>
+<?php while ( have_rows('page_cards') ) : the_row(); ?>
+<?php
+                $page_cards_heading = get_sub_field('page_cards_heading');
+                $page_cards_bg_img = get_sub_field('page_cards_bg_img');
+                $page_cards_description = get_sub_field('page_cards_description');
+                $page_cards_text = get_sub_field('page_cards_text');
+            ?>
+
 <?php get_header(); ?>
 
 <div class="">
-    <?php echo get_the_title(); ?>
-
-    <div class="">
-        <?php the_content(); ?>
-    </div>
+    <img class="page-hero" src="<?php echo $page_hero_img['url']; ?>" />
+    <?= get_the_title(); ?>
 </div>
 
+<div class="page-margin">
+
+
+
+</div>
+
+
+
 <?php get_footer(); ?>
+
+<?php endwhile; ?>
+<?php endif; ?>
