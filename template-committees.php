d312db583cb1a9c2dc7e7ae957f80192c64900bb
Maja Blom Wennerwald Nielsen <70579734+Gajzii@users.noreply.github.com>
2023-11-08 20:42:43 +0100

    rettelser

--- template-committees.php
+++ template-committees.php
@@ -8,22 +8,23 @@
 <div style="padding-top:100px;color:black !important;">
 
 <?php
-
-$committee_pages = get_pages(array(
-    'child_of' => 119, // ID of the parent page
-    'sort_column' => 'post_title',
-));
-
-if (!empty($committee_pages)) {
-    echo '<h3>My Page List :</h3>';
-    foreach ($committee_pages as $page) {
-        $page_title = get_the_title($page);
-        $page_url = get_permalink($page);
-        $page_id = $page->ID;
-        echo '<br />' . $page_title . ' (' . $page_url . ') ID: ' . $page_id;
-    }
-}
+    $committee_pages = get_pages(array(
+        'child_of' => 119, // ID of the parent page
+        'sort_column' => 'post_title',
+    ));
 ?>
+
+<?php if (!empty($committee_pages)) : ?>
+    <h3>My Page List :</h3>
+    <?php foreach ($committee_pages as $page) : ?>
+        <?php 
+            $page_title = get_the_title($page);
+            $page_url = get_permalink($page);
+            $page_id = $page->ID;
+        ?>
+        <br /> <?= $page_title; ?> (<?= $page_url; ?>) ID: <?= $page_id; ?>
+    <?php endforeach; ?>
+<?php endif; ?>
 
 </div>
 
