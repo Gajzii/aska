29a53c300389e3950b952fb1d05747a2f6572315
Camillanrgaard <camilla.nrgaard@yahoo.dk>
2023-11-14 10:24:03 +0100

    Rettelse af template

--- template-about.php
+++ template-about.php
@@ -1,3 +1,9 @@
+<?php
+/**
+ *  Template Name: About
+ */
+?>
+
 <?php get_header(); ?>
 
 <?php $page_hero_img = get_field('page_hero_img'); ?>
@@ -145,6 +151,66 @@
     endwhile;
     ?>
     <?php endif; ?>
+
+
+
+
+    <!-- MANAGEMENT MODAL -->
+    <?php $page_cards_management_text = get_field('page_cards_management_text'); ?>
+
+    <div class="modal modal_multi">
+        <div class="modal-flex modal-flex-management">
+            <div class="page-modal-content page-modal-content-management">
+                <div class="modal-padding">
+                    <span class="close_multi">
+                        <img class="close-icon" alt="Luk modal ikon"
+                            src="<?php echo get_stylesheet_directory_uri(); ?>/assets/media/close-icon.svg" />
+                    </span>
+                    <div class="modal-text-management">
+                        <h4><?= $page_cards_management_heading ?></h4>
+                        <div class="modal-management-scroll">
+                            <p><?= $page_cards_management_text ?></p>
+
+                            <div class="management-modal-section">
+                                <?php if (have_rows('management')) : ?>
+                                <?php while (have_rows('management')) : the_row(); ?>
+                                <?php
+                                    $management_img = get_sub_field('management_img');
+                                    $management_role = get_sub_field('management_role');
+                                    $management_name = get_sub_field('management_name');
+                                    $management_mail = get_sub_field('management_mail');
+                                    $management_responsibility = get_sub_field('management_responsibility');
+                                ?>
+                                <div class="management-member-flex">
+                                    <img class="modal-management-img" src="<?php echo $management_img['url']; ?>" />
+                                    <div class="management-member">
+                                        <h5><?= $management_role ?></h5>
+                                        <p><?= $management_name ?></p>
+
+                                        <div class="management-mail">
+                                            <a target=_blank href="mailto:<?= $management_mail ?>">
+                                                <?= $management_mail ?>
+                                                <img class="mail-icon" alt="email ikon"
+                                                    src="<?php echo get_stylesheet_directory_uri(); ?>/assets/media/mail-icon.svg" />
+
+                                            </a>
+                                        </div>
+                                        <ul>
+                                            <?php foreach ($management_responsibility as $responsibility) : ?>
+                                            <li><?= $responsibility ?></li>
+                                            <?php endforeach; ?>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <?php endwhile; ?>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 </div>
 <?php get_footer(); ?>
