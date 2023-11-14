a561b5dcb50139bac87c31fab7845bb2cf044af9
Camillanrgaard <camilla.nrgaard@yahoo.dk>
2023-11-14 11:55:07 +0100

    -

---
+++ template-merchandise.php
@@ -0,0 +1,18 @@
+<?php   
+/**
+ *  Template Name: Merchandise */
+?>
+
+<?php get_header(); ?>
+<div class="page-hero-container">
+    <img class="page-hero" src="<?php echo $committees_hero_img['url']; ?>" />
+    <h1 class="page-hero-content"><?= get_the_title(); ?></h1>
+</div>
+
+<div class="page-margin">
+
+</div>
+
+
+
+<?php get_footer(); ?>
