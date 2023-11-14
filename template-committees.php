209a47c0ec0b497fa8f2d79d91f9ba88f901775c
Maja Blom Wennerwald Nielsen <70579734+Gajzii@users.noreply.github.com>
2023-11-14 13:10:50 +0100

    rettelser

--- template-committees.php
+++ template-committees.php
@@ -5,27 +5,6 @@
 
 <?php get_header(); ?>
 
-<?php
-    $committee_pages = get_pages(array(
-        'child_of' => 23,
-        'sort_column' => 'post_title',
-    ));
-?>
-
-<?php if (!empty($committee_pages)) : ?>
-<?php foreach ($committee_pages as $page) : ?>
-<?php 
-            $page_title = get_the_title($page);
-            $page_url = get_permalink($page);
-            $page_id = $page->ID;
-        ?>
-<br /> <?= $page_title; ?> (<?= $page_url; ?>) ID: <?= $page_id; ?>
-<?php endforeach; ?>
-<?php endif; ?>
-
-</div>
-
-
 <?php $committees_hero_img = get_field('committees_hero_img'); ?>
 
 <!-- PAGE HERO -->
@@ -38,26 +17,31 @@
     <div class="page-cards-flex">
 
         <!-- PAGE CARD CONTENT -->
-        <?php if ( have_rows('committees') ) : ?>
-        <?php while ( have_rows('committees') ) : the_row(); ?>
+        
         <?php
-                $committees_heading = get_sub_field('committees_heading');
-                $committees_bg_img = get_sub_field('committees_bg_img');
-                $committees_description = get_sub_field('committees_description');
-                $committees_link = get_sub_field('committees_link');
-            ?>
+            $committee_pages = get_pages(array(
+                'child_of' => 119,
+                'sort_column' => 'post_title',
+            ));
+        ?>
+
+        <?php if (!empty($committee_pages)) : ?>
+        <?php foreach ($committee_pages as $page) : ?>
+        <?php 
+            $page_title = get_the_title($page);
+            $page_url = get_permalink($page);
+            $page_img = get_field('page_hero_img', $page);
+        ?>
 
         <div class="page-card" ontouchmove=""
-            style="background-image: url('<?php echo $committees_bg_img['url']; ?>');">
+            style="background-image: url('<?= $page_img['url']; ?>');">
             <div class="page-card-heading" ontouchmove="">
                 <h4>
-                    <?= $committees_heading ?>
+                    <?= $page_title; ?>
                 </h4>
                 <div class="page-card-details">
-                    <p class="page-card-description"><?= $committees_description ?></p>
-
-
-                    <a href="href=<?= $committees_link['url']; ?>" class="secondary-btn-border page-read-more-btn">
+                    <br>
+                    <a href="href=<?= $page_url; ?>" class="secondary-btn-border page-read-more-btn">
                         <button class="readMore_multi secondary-btn btn-text-secondary page-read-more-btn-secondary">
                             Læs mere
                             <img class="arrow-icon" alt="Pil ikon til højre"
@@ -67,8 +51,13 @@
                 </div>
             </div>
         </div>
-        <?php endwhile; ?>
+
+        <?php endforeach; ?>
         <?php endif; ?>
+
+        </div>
+
+       
 
     </div>
 </div>
